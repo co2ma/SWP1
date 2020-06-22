92d9e13f64cfc8aef6968d5421d7ea0dd3292784
co2ma <co2matsign@kookmin.ac.kr>
2020-06-23 01:01:20 +0900

    Issue #1

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+    <html>
+        <body>
+            <form action="">
+                a = <input type="number" name="a"> 
+                b = <input type="number" name="b"><br><br>
+                <input type="submit"><br>
+                a + b = {}
+                <br>
+                a * b = {}
+            </form>
+        </body>
+    </html>
+"""
+
